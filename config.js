a06b33cea95cabb5a2c86905655555092dff749d
Naif Almohammdi <almohammdin@gmail.com>
2026-08-19 00:42:39 +0300

    Configure direct inquiry email

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SANAAM_CONFIG = {
-  version: '1.0.0',
   basePath: '/SANAAM-Lubricants/',
-  contact: { whatsapp: '', email: '', phone: '' },
+  contact: { inquiryEmail: 'naif123456@gmail.com' },
+  formSubmitEndpoint: 'https://formsubmit.co/ajax/naif123456@gmail.com',
   external: { aramcoBaseOils: 'https://www.aramco.com/en/what-we-do/energy-products/refined-products/base-oils' }
 };
