d26481b709f90c41b527524144f1a829f60b7a0f
Naif Almohammdi <almohammdin@gmail.com>
2026-08-18 23:51:52 +0300

    Trigger Pages deployment

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.SANAAM_CONFIG = {
+  version: '1.0.0',
   basePath: '/SANAAM-Lubricants/',
   contact: { whatsapp: '', email: '', phone: '' },
   external: { aramcoBaseOils: 'https://www.aramco.com/en/what-we-do/energy-products/refined-products/base-oils' }
