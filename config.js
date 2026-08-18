9382ce09e2d22b3cf1b218e5562c04ba0cb2682e
Naif Almohammdi <almohammdin@gmail.com>
2026-08-18 23:50:17 +0300

    Build SANAAM multilingual export catalogue

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SANAAM_CONFIG = {
+  basePath: '/SANAAM-Lubricants/',
+  contact: { whatsapp: '', email: '', phone: '' },
+  external: { aramcoBaseOils: 'https://www.aramco.com/en/what-we-do/energy-products/refined-products/base-oils' }
+};
